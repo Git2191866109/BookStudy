8908abf3740985407293edfe2f1b7b1968e8e307
PC <694317828@qq.com>
2019-08-20 19:06:02 +0800

    20190820-1906

---
+++ else/JavaPro/src/Test3.java
@@ -0,0 +1,17 @@
+/**
+ * @author: Li Tian
+ * @contact: 694317828@qq.com
+ * @software: pycharm
+ * @file: Test3.java
+ * @time: 2019/8/20 17:48
+ * @desc:
+ */
+
+public class Test3 {
+    public static void main(String[] args){
+        int score = 80;
+        String type = score<60? "不及格":"及格";
+        System.out.println(type);
+
+    }
+}
