24e30822852053b6f90c3cfaa784608bf1de6469
PC <694317828@qq.com>
2019-08-25 20:33:42 +0800

    20190825-2033

--- else/JavaPro/src/Test3.java
+++ else/JavaPro/src/Test3.java
@@ -13,5 +13,18 @@ public class Test3 {
         String type = score<60? "不及格":"及格";
         System.out.println(type);
 
+        double x = Math.random();
+        System.out.println(x);
+        System.out.println((int)(6*x + 1));
+
+
+        outer: for (int i = 101; i < 150; i++) {
+            for (int j = 2; j < i / 2; j++) {
+                if (i % j == 0){
+                    continue outer;
+                }
+            }
+            System.out.print(i + "  ");
+        }
     }
 }
