e6d2e356e0b5dbdaef5e2d4618e1f83ebca22279
PC <694317828@qq.com>
2019-08-28 20:41:42 +0800

    20190828-2041

--- else/JavaPro/src/Test4.java
+++ else/JavaPro/src/Test4.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * @author: Li Tian
  * @contact: 694317828@qq.com
@@ -9,7 +11,38 @@
 
 public class Test4 {
     public static void main(String[] args){
-        Test4 x = new Test4();
-        System.out.println(x instanceof Object);
+
+        User u1 = new User(1000, "小高", "123456");
+        User u2 = new User(1000, "小高高", "123456");
+
+        System.out.println(u1==u2);
+        System.out.println(u1.equals(u2));
+        System.out.println(u1.hashCode());
+    }
+}
+
+class User{
+    int id;
+    String name;
+    String pwd;
+
+    public User(int id, String name, String pwd) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, pwd);
     }
 }
