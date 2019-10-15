3ae25c75afdd0f2c7fe535db481e70643c13f0cc
PC <694317828@qq.com>
2019-10-15 18:19:59 +0800

    新增IO四大类

--- else/JAVAPro/src/TestIO3.java
+++ else/JAVAPro/src/TestIO3.java
@@ -13,7 +13,7 @@ public class TestIO3 {
     public static void main(String[] args){
         // mkdir()：确保上级目录存在，不存在则创建失败
         // mkdirs()：上级目录可以不存在，不存在则一同创建
-        File dir = new File("D:/as/asd/d");
+        File dir = new File("D:/");
 
         boolean flag1 = dir.mkdir();
         boolean flag2 = dir.mkdirs();
@@ -41,5 +41,22 @@ public class TestIO3 {
         // 递归：方法自己调用自己
         // 递归头：何时结束递归
         // 递归体：重复调用
+        printName(dir, 0);
+
+    }
+
+    public static void printName(File src, int deep){
+        /* 打印子孙级目录和文件的名称 */
+        for(int i=0; i<deep; i++){
+            System.out.print("-");
+        }
+        System.out.println(src.getName());
+        if(null == src || !src.exists()){
+            return;
+        } else if(src.isDirectory()){
+            for(File s: src.listFiles()){
+                printName(s, deep + 1);
+            }
+        }
     }
 }
