3c2d975c4e129fac81dcfb5f6839c76b72e26ae0
temp1 <694317828@qq.com>
2019-10-18 16:42:48 +0800

    一些文件输入输出流，字节数组输入输出流的实现，装饰器原理

--- else/JAVAPro/src/TestFileCopy.java
+++ else/JAVAPro/src/TestFileCopy.java
@@ -10,19 +10,53 @@ import java.io.*;
  */
 
 public class TestFileCopy {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        copy("test.png", "copy_test.png");
+    }
+
+    public static void copy(String srcPath, String destPath){
         // 1. 创建源
         // 源头
-        File src = new File("abc.txt");
-        File dest = new File("dest.txt");
+        File src = new File(srcPath);
+        File dest = new File(destPath);
         // 2. 选择流
         InputStream is = null;
         OutputStream os = null;
         try{
             is = new FileInputStream(src);
             os = new FileOutputStream(dest, true);
-            //
+            // 3. 操作（分段读取）
+            // 缓冲容器
+            byte[] flush = new byte[1024];
+            // 接受长度
+            int len = -1;
+            while((len=is.read(flush)) != -1){
+                // 字节数组 --> 字符串（解码）
+                String str = new String(flush, 0, len);
+                os.write(flush, 0, len);
+            }
+            os.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源 先打开的后关闭
+            try{
+                if(null != os){
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            try{
+                if(null != is){
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
