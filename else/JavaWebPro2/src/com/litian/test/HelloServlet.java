b7ed2e2a9545c048634529fee99d5af45c8d497f
李英俊的笔记本 <694317828@qq.com>
2020-04-12 15:22:09 +0800

    学到P9

--- else/JavaWebPro2/src/com/litian/test/HelloServlet.java
+++ else/JavaWebPro2/src/com/litian/test/HelloServlet.java
@@ -2,6 +2,7 @@ package com.litian.test;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 
 /**
@@ -21,6 +22,8 @@ public class HelloServlet implements Servlet {
 
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
+        // 1. ServletConfig
+        // 针对Servlet的初始化参数
         System.out.println("初始化中...");
         String user = servletConfig.getInitParameter("user");
         System.out.println("user: " + user);
@@ -32,6 +35,47 @@ public class HelloServlet implements Servlet {
         }
         String servletName = servletConfig.getServletName();
         System.out.println(servletName);
+
+        // 2. ServletContext
+        // 针对app的初始化参数，相较于上面，该参数适用于所有Servlet，相当于全局的参数
+        // 2.1 获取ServletContext对象
+        ServletContext sc = servletConfig.getServletContext();
+        // 获取WEB应用程序的初始化参数
+        String driver = sc.getInitParameter("driver");
+        System.out.println("driver: " + driver);
+        Enumeration<String> names2 = sc.getInitParameterNames();
+        while(names2.hasMoreElements()){
+            String name = names2.nextElement();
+            String value = sc.getInitParameter(name);
+            System.out.println(name + "-->" + value);
+        }
+
+        // 2.2 获取当前WEB应用的某一个文件在服务器上的绝对路径：
+        String realPath = sc.getRealPath("/index.jsp");
+        System.out.println(realPath);
+
+        // 2.3 获取当前WEB应用的名称
+        String contextPath = sc.getContextPath();
+        System.out.println(contextPath);
+
+        // 2.4 获取当前WEB应用的某一个文件对应的输入流
+        // path的 / 响度与当前WEB应用的根目录
+        try {
+            ClassLoader cl = getClass().getClassLoader();
+            InputStream is = cl.getResourceAsStream("jdbc.properties");
+            System.out.println("1. " + is);
+
+            InputStream is2 = sc.getResourceAsStream("jdbc.properties");
+            System.out.println("2. " + is2);
+
+            InputStream is3 = sc.getResourceAsStream("/WEB-INF/classes/jdbc.properties");
+            System.out.println("3. " + is3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 2.5 和attribute相关的几个方法
+
     }
 
     @Override
